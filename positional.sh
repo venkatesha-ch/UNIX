5254db9e0e40abf4dc57a0cedb574081a57892d0
Venkatesh ch <53754562+venkatesha-ch@users.noreply.github.com>
2020-10-06 15:39:03 +0530

    Add files via upload

--- positional.sh
+++ positional.sh
@@ -3,8 +3,8 @@ then
 echo "No parameters passed"
 elif test $# -eq 2
 then
-echo `ls -l $1` || echo "$1 Not found "
-echo `ls -l $2` || echo "$2 Not found "
+echo `stat $1` || echo "$1 Not found "
+echo `stat $2` || echo "$2 Not found "
 else
 echo "Pass 2 parameters only"
 fi
